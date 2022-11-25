6a97c257dcbe06ffbaebd49d2d7dc34c0a04b7e2
Leo_sailor <leo.yates@reddamhouse.org.uk>
2022-11-25 13:10:16 +0000

    update 25th nov

--- ELO.py
+++ ELO.py
@@ -2,6 +2,6 @@ class EloCalculations:
     def __init__ (self):
         print('elo calculations are now imported and running')
     def calc (self,rat1,rat2,result,k):
-        pred = 5
+        pred = 1/(1+10**(rat2-rat1))
         change = 5
         return [rat1-change,rat2+change]
--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from ELO import EloCalculations
 
 #initzalizations
 elo = EloCalculations()
+
+
 print('new ratings are{}'.format(elo.calc(1500,16000,1,24)))
