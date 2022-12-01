b2bb159aaa7798d17ab9189901d1f213bec494bb
Leo_sailor <leo.yates@reddamhouse.org.uk>
2022-12-01 10:43:43 +0000

    pt1 of file clean

--- csvcode.py
+++ csvcode.py
@@ -1,14 +1,14 @@
 import sys
 import csv
 import pandas as pd
-
+import os
 
 class Csvcode:
     def __init__(self, universe):
         if universe.upper() == 'N':
             name = input('please enter your new ranking universe name')
             name.lower().strip()
-            file = (name, '.csv')
+            file = (sys.path[0], '\\universes\\', name, '.csv')
             universe = name
 
             with open(''.join(file), 'w', newline='') as csvfile:
@@ -25,7 +25,7 @@ class Csvcode:
                 starting = int(input('What would you like the average rating of this '
                                      'universe to be?(450-3100)(default: 1500)'))
 
-            with open('host.csv', 'w', newline='') as csvfile:
+            with open('universes/host.csv', 'w', newline='') as csvfile:
                 spamwriter = csv.writer(csvfile, delimiter=',',
                                         quotechar=',', quoting=csv.QUOTE_MINIMAL)
                 spamwriter.writerow([name, 0, starting, (starting/5+100)])
@@ -33,8 +33,9 @@ class Csvcode:
             print('{}.csv has been created'.format(name))
 
         try:
-            file = (universe, '.csv')
+            file = (sys.path[0], '\\universes\\', universe, '.csv')
             self.uniloc = ''.join(file)
+
             with open(''.join(file), newline='') as csvfile:
                 spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
                 x = 0
